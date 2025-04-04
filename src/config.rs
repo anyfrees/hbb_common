69951e30521da9b6a2a123cfde37616eb9d1e82c
anyfrees <131247791+anyfrees@users.noreply.github.com>
2025-04-04 19:48:09 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,11 +98,11 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.0vk.com"];
+pub const RS_PUB_KEY: &str = "137858006";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
-pub const RELAY_PORT: i32 = 21117;
+pub const RELAY_PORT: i32 = 22222;
 
 macro_rules! serde_field_string {
     ($default_func:ident, $de_func:ident, $default_expr:expr) => {
